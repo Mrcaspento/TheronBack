c00730e62bcb1628631ed10ca21720f0e1324a06
Caspento <Mrcaspento@gmail.com>
2021-02-11 15:11:27 -0500

    init push

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const session = require("express-session");
+const mongoose = require("mongoose");
+const routes = require("./routes")
+const PORT = process.env.PORT || 3001;
+const app = express();
+const passport = require('passport');
+
+//Middle ware for login/ Business/Org
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({ secret: 'i@N7yVv*62#9' }));
+app.use(passport.initialize());
+
+app.use(passport.session())
+
+//adding routes to the API and view
+app.use(routes);
+
+// if (process.env.NODE_ENV == "production") {
+//     app.use(express.static("/client/build"));
+//}
+if (process.env.NODE_ENV) {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+ 
+//   mongodb+srv://root:d34th_f0r_Pizza_tim3r@cluster0.fefwt.mongodb.net/TestServerDb?retryWrites=true&w=majority
+// connecnt to Mongo Db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/TestServerDb",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+
+// start the API server
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
